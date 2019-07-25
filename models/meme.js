474a71ba4e4e4ae514d6ff2574dae3002e6d2f3a
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-07-25 09:36:23 -0400

    created meme.js

---
+++ models/meme.js
@@ -0,0 +1,78 @@
+/* 
+ * Place all functions, classes, and/or DB schemas here for a single 
+ * model.
+ */
+
+/* Step 1
+ *
+ * TODO: import mongoose connection
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const mongoose = require('./connection.js')
+
+/* Step 1 alternative
+ *
+ * TODO: make a global variable to act as an in memory database. 
+ * NOTE: doing this WILL NOT persist your data and you will loose
+ * your data once you stop running your server.
+ *
+ */
+// global.sampleModel = [];
+
+/* Step 2
+ *
+ * TODO: create model schema 
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const MemeSchema = new mongoose.Schema({
+ name: String
+})
+
+/* Step 3
+ *
+ * TODO: create collection API
+ * NOTE: skip this if you are not using mongoose
+ *
+ */
+const MemeCollection = mongoose.model('Meme', MemeSchema)
+
+/* Step 4
+ *
+ * TODO: delete this it's just a sample
+ *
+ */
+function getAllMemes() {
+  return MemeCollection.find()
+}
+
+function getMeme(memeId) {
+    return MemeCollection.findById(memeId)
+}
+
+function addNewMeme(memeObject) {
+    return MemeCollection.create(memeObject)
+}
+
+function updateMeme(memeId, updateMeme) {
+    return MemeCollection.findByIdAndUpdate(memeId, updateMeme)
+}
+
+function deleteMeme(memeId) {
+    return MemeCollection.findByIdAndDelete(memeId)
+}
+
+
+/* Step 5
+ *
+ * TODO: export all functions from this file by adding their names as keys to this
+ * object
+ */
+module.exports = {
+  getAllMemes,
+  getMeme,
+  addNewMeme,
+  updateMeme,
+  deleteMeme
+}
