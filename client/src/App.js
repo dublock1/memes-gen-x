5cdac221d336dedfd22e49ee4e68734a8090faf9
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-07-29 11:47:52 -0400

    added User route

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import HomePage from './components/HomePage'
 import Memes from './components/Memes'
 import NavBar from './components/NavBar'
+import Users from './components/Users'
+import User from './components/User'
 import './App.css';
+import Meme from './components/Meme';
 
 function App() {
   return (
     <div className="App">
+      <NavBar/>
       <Router>
         <Switch>
-          <Route exact path="/" component={Memes}/>          
+          <Route exact path="/" component={HomePage}/>   
+          <Route exact path="/users" component={Users}/>
+          <Route path="/users/:userId" component={User}/>
+          <Route path="/memes/:memeId" component={Meme}/>       
         </Switch>
       </Router>
     </div>
