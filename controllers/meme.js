d452cf774daa1de962384a1ba508af858d1bdf81
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-07-29 13:20:08 -0400

    cleaned code

--- controllers/meme.js
+++ controllers/meme.js
@@ -1,41 +1,13 @@
-/* Step 1 import express
- *
- */
+
 const express = require('express')
 
-/* Step 2
- *
- * Import the api files from the models
- *
- * TODO: change the file path to the models file you'll need to use.
- * TODO: rename this from `templateApi` to something more sensible (e.g:
- * `shopsAPI`)
- *
- * NOTE: You may need to import more than one API to create the 
- * controller you need.
- * 
- */
+
 const memeApi = require('../models/meme.js')
 
-/* Step 3 
- * 
- * Create a new router.
- *
- * the router will "contain" all the request handlers that you define in this file.
- * TODO: rename this from templateRouter to something that makes sense. (e.g:
- * `shopRouter`)
- */
+
 const memeRouter = express.Router()
 
-/* Step 4
- * 
- * TODO: Put all request handlers here
- */
 
-/* Step 5
- *
- * TODO: delete this handler; it's just a sample
- */ 
 memeRouter.get('/', (req, res) => {
     memeApi.getAllMemes()
     .then((memes) => {
@@ -74,11 +46,7 @@ memeRouter.delete('/:memeId', (req, res) => {
     })
 })
 
-/* Step 6
- *
- * Export the router from the file.
- *
- */
+
 module.exports = {
   memeRouter
 }
