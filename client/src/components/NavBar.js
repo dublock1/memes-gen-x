416882cfcd7153a65dd59e551f751f5fef042520
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-07-29 13:56:42 -0400

    added link to user page

--- client/src/components/NavBar.js
+++ client/src/components/NavBar.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react'
-import {Button, Navbar} from 'react-bootstrap';
+import React, { Component } from "react";
+import { Button, Navbar } from "react-bootstrap";
+import { Link } from "react-router-dom";
 
 export default class NavBar extends Component {
-    render() {
-        return (
-            <Navbar bg="dark" variant="dark" sticky="top" className="justify-content-between" activeKey="/home">
-                <h1>Meme Gen X</h1>
-                <Button >LOGIN</Button>
-            </Navbar>
-            
-        )
-    }
+  render() {
+    return (
+      <Navbar
+        bg="dark"
+        variant="dark"
+        sticky="top"
+        className="justify-content-between"
+        activeKey="/home"
+      >
+        <h1>Meme Gen X</h1>
+        <a href="/users">
+          <Button>LOGIN</Button>
+        </a>
+      </Navbar>
+    );
+  }
 }
-
