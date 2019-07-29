3081ca535a48a6247dff423db89113fdf814551b
Robert Lockwood-Haskins <rob.haskins11@gmail.com>
2019-07-29 11:59:39 -0400

    added edit and delete button

---
+++ client/src/components/User.js
@@ -0,0 +1,95 @@
+import React, { Component } from "react";
+import axios from "axios";
+import { Redirect } from "react-router-dom";
+import { Button } from "react-bootstrap";
+export default class User extends Component {
+  state = {
+    isEditFormDisplayed: false,
+    redirectToHome: false,
+    newUser: {
+      firstName: "",
+      lastName: "",
+      email: ""
+    }
+  };
+
+  componentDidMount() {
+    axios.get(`/api/users/${this.props.match.params.userId}`).then(res => {
+      this.setState({ newUser: res.data });
+    });
+  }
+
+  handleInputChange = event => {
+    const copiedUser = { ...this.state.newUser };
+    copiedUser[event.target.name] = event.target.value;
+
+    this.setState({ newUser: copiedUser });
+  };
+
+  handleSubmit = event => {
+    event.preventDefault();
+
+    axios
+      .put(`/api/users/${this.state.newUser._id}`, this.state.newUser)
+      .then(res => {
+        this.setState({
+          newUser: res.data,
+          isEditFormDisplayed: false,
+          redirectToHome: true
+        });
+      });
+  };
+
+  handleToggleEditForm = event => {
+    this.setState(state => {
+      return { isEditFormDisplayed: !state.isEditFormDisplayed };
+    });
+  };
+
+  handleDeleteCreature = () => {
+    axios.delete(`/api/users/${this.state.newUser._id}`).then(() => {
+      this.setState({ redirectToHome: true });
+    });
+  };
+
+  render() {
+    if (this.state.redirectToHome) {
+      return <Redirect to="/users" />;
+    }
+    return this.state.isEditFormDisplayed ? (
+      <form onSubmit={this.handleSubmit}>
+        <label htmlFor="new-user-firstname">First Name</label>
+        <input
+          type="text"
+          id="new-user-firstname"
+          name="firstName"
+          onChange={this.handleInputChange}
+          value={this.state.newUser.firstName}
+        />
+        <label htmlFor="new-user-lastname">Last Name</label>
+        <input
+          type="text"
+          id="new-user-lastname"
+          name="lastName"
+          onChange={this.handleInputChange}
+          value={this.state.newUser.lastName}
+        />
+        <label htmlFor="new-user-email">User Email</label>
+        <input
+          type="text"
+          id="new-user-email"
+          name="email"
+          onChange={this.handleInputChange}
+          value={this.state.newUser.email}
+        />
+
+        <input type="submit" value="Update User" />
+      </form>
+    ) : (
+      <div>
+        <Button onClick={this.handleToggleEditForm}>Edit Creature</Button>
+        <Button onClick={this.handleDeleteCreature}>Delete Creature</Button>
+      </div>
+    );
+  }
+}
